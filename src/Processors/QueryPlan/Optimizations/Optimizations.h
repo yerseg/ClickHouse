e8115370161b1db83e32250f827d44b279a44350
vdimir <vdimir@clickhouse.com>
2023-02-22 10:52:25 +0000

    Move join related code for optimize read in order in separate file

--- src/Processors/QueryPlan/Optimizations/Optimizations.h
+++ src/Processors/QueryPlan/Optimizations/Optimizations.h
@@ -107,6 +107,8 @@ using Stack = std::vector<Frame>;
 void optimizePrimaryKeyCondition(const Stack & stack);
 void optimizeReadInOrder(QueryPlan::Node & node, QueryPlan::Nodes & nodes);
 void optimizeAggregationInOrder(QueryPlan::Node & node, QueryPlan::Nodes &);
+
+/// Insert sorting step before join if needed or use read in order
 void applyOrderForJoin(QueryPlan::Node & node, QueryPlan::Nodes & nodes, const QueryPlanOptimizationSettings & optimization_settings);
 
 /// Enable memory bound merging of aggregation states for remote queries
